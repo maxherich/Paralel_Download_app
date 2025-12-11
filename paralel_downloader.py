ea1ebd40ed2c41dd483bc60c4c5c19ee03f06e78
Max Herich <max.herich@gmail.com>
2025-12-11 20:08:53 +0100

    Add create_widgets Method

--- paralel_downloader.py
+++ paralel_downloader.py
@@ -14,3 +14,38 @@ class ParallelDownloader:
         self.is_downloading = False
         self.total_size = 0
         self.downloaded_bytes = 0
+
+        self.create_widgets()
+
+
+    def create_widgets(self):
+        frame_url = tk.LabelFrame(self.root, text="File URL", padx=10, pady=10)
+        frame_url.pack(fill="x", padx=10, pady=5)
+
+        entry_url = tk.Entry(frame_url, textvariable=self.url_var)
+        entry_url.pack(fill="x")
+
+        frame_path = tk.LabelFrame(self.root, text="Save PATH", padx=10, pady=10)
+        frame_path.pack(fill="x", padx=10, pady=5)
+
+        path_container = tk.Frame(frame_path)
+        path_container.pack(fill="x")
+
+        entry_path = tk.Entry(path_container, textvariable=self.path_var)
+        entry_path.pack(side="left", fill="x", expand=True)
+
+        btn_browse = tk.Button(path_container, text="Choose PATH", command=self.browse_file)
+        btn_browse.pack(side="right", padx=(5, 0))
+
+        frame_control = tk.Frame(self.root, padx=10, pady=10)
+        frame_control.pack(fill="x", padx=10)
+
+        self.btn_start = tk.Button(frame_control, text="DOWNLOAD", command=self.start_download_thread,
+                                   font=("Arial", 10))
+        self.btn_start.pack(fill="x", pady=5)
+
+        self.progress_bar = ttk.Progressbar(frame_control, variable=self.progress_var, maximum=100)
+        self.progress_bar.pack(fill="x", pady=10)
+
+        self.lbl_status = tk.Label(frame_control, textvariable=self.status_var, font=("Arial", 10))
+        self.lbl_status.pack()
